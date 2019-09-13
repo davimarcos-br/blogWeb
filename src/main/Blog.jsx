a4536071205973634f7695072a153f86f27680dd
davimarcos-br <braginha@gmail.com>
2019-09-13 16:14:22 -0300

    paginação

--- src/main/Blog.jsx
+++ src/main/Blog.jsx
@@ -11,7 +11,9 @@ import './Blog.css'
 const initialState = {
     disc: null,
     serie: null,
-    professor: null
+    professor: null,
+    limit:2,
+    contador: 2
 
 }
 
@@ -20,102 +22,120 @@ const initialState = {
 class Blog extends Component {
 
 
-state = {...initialState}
-constructor(props){
-    super(props)
+    state = { ...initialState }
+    constructor(props) {
+        super(props)
 
-    this.filtroDisc = this.filtroDisc.bind(this)
-    this.filtroSerie = this.filtroSerie.bind(this)
-    this.filtroAutor = this.filtroAutor.bind(this)
-    
-}
+        this.filtroDisc = this.filtroDisc.bind(this)
+        this.filtroSerie = this.filtroSerie.bind(this)
+        this.filtroAutor = this.filtroAutor.bind(this)
+        this.filtroLimit = this.filtroLimit.bind(this)
+
+    }
 
 
-btnClick(btn){
-    
-        if(btn =='disc'){this.setState({disc:null})}
-        else if(btn =='serie'){this.setState({serie:null})}
-        else if(btn =='professor'){this.setState({professor:null})}
-}
+    btnClick(btn) {
 
-filtroDisc(value){
-    this.setState({disc:value})
-}
+        if (btn == 'disc') { this.setState({ disc: null }) }
+        else if (btn == 'serie') { this.setState({ serie: null }) }
+        else if (btn == 'professor') { this.setState({ professor: null }) }
+    }
 
-filtroSerie(value){
-    this.setState({serie:value})
-}
+    filtroDisc(value) {
+        this.setState({ disc: value })
+    }
 
-filtroAutor(value){
-    this.setState({professor:value})
-}
+    filtroSerie(value) {
+        this.setState({ serie: value })
+    }
 
-toastDisc() {  
-    const visivel = this.state.disc != null ? "visible" : "invisible" 
-    return (
+    filtroAutor(value) {
+        this.setState({ professor: value })
+    }
+
+    filtroLimit() {
+        //console.log(this.state.limit)
+        let limit = this.state.limit + this.state.contador
+       // console.log(limit)
+        this.setState({ limit })
+    }
+
+    toastDisc() {
+        const visivel = this.state.disc != null ? "visible" : "invisible"
+        return (
             <div className={visivel + "  d-flex flex-row btnclear"}>
-                           <p class="my-auto ml-1">{this.state.disc}</p>
-                            <button onClick={e => this.btnClick('disc')} type="button" className="btn  btn-sm">
-                            <span >&times;</span>
-                            </button>
-                            
-            </div>
-            
-      )
-  }
+                <p class="my-auto ml-1">{this.state.disc}</p>
+                <button onClick={e => this.btnClick('disc')} type="button" className={visivel +" btn  btn-sm"}>
+                    <span >&times;</span>
+                </button>
 
-  toastSerie() {  
-    const visivel = this.state.serie != null ? "visible" : "invisible" 
-    return (
+            </div>
+
+        )
+    }
+
+    toastSerie() {
+        const visivel = this.state.serie != null ? "visible" : "invisible"
+        return (
             <div className={visivel + "  d-flex flex-row btnclear"}>
-                           <p class="my-auto ml-1">{this.state.serie}</p>
-                            <button onClick={e => this.btnClick('serie')} type="button" className="btn  btn-sm">
-                            <span >&times;</span>
-                            </button>
-                            
-            </div>
-            
-      )
-  }
+                <p class="my-auto ml-1">{this.state.serie}</p>
+                <button onClick={e => this.btnClick('serie')} type="button" className="btn  btn-sm">
+                    <span >&times;</span>
+                </button>
 
-  toastProfessor() {  
-    const visivel = this.state.professor != null ? "visible" : "invisible" 
-    return (
+            </div>
+
+        )
+    }
+
+    toastProfessor() {
+        const visivel = this.state.professor != null ? "visible" : "invisible"
+        return (
             <div className={visivel + " d-flex flex-row btnclear"}>
-                           <p class="my-auto ml-1">{this.state.professor}</p>
-                            <button onClick={e => this.btnClick('professor')} type="button" className="btn  btn-sm">
-                            <span >&times;</span>
-                            </button>
-                            
+                <p class="my-auto ml-1">{this.state.professor}</p>
+                <button onClick={e => this.btnClick('professor')} type="button" className="btn  btn-sm">
+                    <span >&times;</span>
+                </button>
+
             </div>
-            
-      )
-  }
-  
+
+        )
+    }
+
 
     render() {
         return (
             <Container>
-            <Row>
-                <Col sm={3} className="d-none d-sm-block">
-                    <Nav n="teste" sClick={this.filtroSerie}/></Col>
-                <Col sm={9} className="" >
-                <div className="d-flex flex-row border-bottom py-1">
-                                {this.toastProfessor()}  
-                                {this.toastDisc()}  
-                                {this.toastSerie()}   
+                <Row>
+                    <Col sm={3} className="d-none d-lg-block">
+                        <Nav n="teste" sClick={this.filtroSerie} /></Col>
+                    <Col sm={9} className="" >
+                        <div class="card text-center m-1">
+                            <div class="card-header d-flex flex-row p-1">
+                                {this.toastProfessor()}
+                                {this.toastDisc()}
+                                {this.toastSerie()} 
+                            </div>
+                            <div class="card-body p-1">
+                            <Post autor={this.state.professor}
+                            pClick={this.filtroAutor}
+                            disciplina={this.state.disc}
+                            dClick={this.filtroDisc}
+                            serie={this.state.serie}
+                            sClick={this.filtroSerie}
+                            limit={this.state.limit} />
+                          
+                            </div>
+                            <div class="card-footer text-muted">
+                                <button onClick={e=> this.filtroLimit()}>Mais</button>
                         </div>
-                    
-                    <Post   autor={this.state.professor} 
-                            pClick={this.filtroAutor} 
-                            disciplina={this.state.disc} 
-                            dClick={this.filtroDisc}  
-                            serie={this.state.serie} 
-                            sClick={this.filtroSerie}/> 
-                </Col>
+                        </div>
 
-            </Row>
-          </Container>
+                        
+                    </Col>
+
+                </Row>
+            </Container>
         )
     }
 }
--- src/main/components/Post.jsx
+++ src/main/components/Post.jsx
@@ -7,76 +7,95 @@ import Jumbotron from 'react-bootstrap/Jumbotron'
 import Autor from './Autor'
 import Tag from './Tag'
 
-const initialState = { list: [] }
+const initialState = {
+
+        list: []
+}
 
 
 class Post extends Component {
         state = { ...initialState }
-        constructor(props){
+        constructor(props) {
                 super(props)
         }
 
         componentWillMount() {
-                let query = firebaseDatabase.ref('post')
-                        .limitToLast(10)
-        
-                query.on('value', dataSnapshot => {
-                        let items = []
-                        dataSnapshot.forEach(childSnapshot => {
-                                let item = childSnapshot.val()
-                                item['key'] = childSnapshot.key
-                                items.push(item)
-                        })
-                        this.setState({ list: items })
-                })
+                this.getData()
+
+
         }
 
-        filterAutor = lista =>  this.props.autor != null ?  lista.pautor == this.props.autor: true
-        filterDisciplina = lista =>  this.props.disciplina != null ?  lista.pdisciplina == this.props.disciplina: true
-        filterSerie = lista =>  this.props.serie != null ?  lista.pseries.includes(this.props.serie): true
+        getData = () => {
+                let query = firebaseDatabase.ref('post')
+                      
+                
 
-        renderTag(autor, date, disciplina,series=[]){
+
+             
+                query.on('value')
+                        .then(dataSnapshot => {
+                        let items = []
+                        dataSnapshot
+                                .forEach(childSnapshot => {
+                                        let item = childSnapshot.val()
+                                        item['key'] = childSnapshot.key
+                                        items.push(item)
+                                })
+                                items.reverse()
+                        this.setState({ list: items })
+                })
+
+        };
+
+        filterAutor = lista => this.props.autor != null ? lista.pautor == this.props.autor : true
+        filterDisciplina = lista => this.props.disciplina != null ? lista.pdisciplina == this.props.disciplina : true
+        filterSerie = lista => this.props.serie != null ? lista.pseries.includes(this.props.serie) : true
+
+        renderTag(autor, date, disciplina, series = []) {
                 return (
                         <div className="d-flex flex-row border-top py-1 mt-auto">
-                        <button type="button"
-                                className="btn TagAutor Tag d-block d-sm-none px-2"
-                                onClick={e => this.props.pClick(autor)}>{autor}</button> 
-                        <button 
-                                type="button" 
-                                className={disciplina + ' btn Tag'}
-                                onClick={e => this.props.dClick(disciplina)}>{disciplina}</button>
-                        <p className="Tag Data">{date}</p>
-                         {series.map((str) => <button 
-                                                type="button" 
-                                                className={str + ' btn Tag Serie'}
-                                                onClick={e => this.props.sClick(str)}>{str}</button>)}
-                         </div>
-                        )
+                                <button type="button"
+                                        className="btn TagAutor Tag d-block d-sm-none px-2"
+                                        onClick={e => this.props.pClick(autor)}>{autor}</button>
+                                <button
+                                        type="button"
+                                        className={disciplina + ' btn Tag'}
+                                        onClick={e => this.props.dClick(disciplina)}>{disciplina}</button>
+                                <p className="Tag Data d-none d-lg-block">{date}</p>
+                                {series.map((str) => <button
+                                        type="button"
+                                        className={str + ' btn Tag Serie'}
+                                        onClick={e => this.props.sClick(str)}>{str}</button>)}
+                        </div>
+                )
         }
 
         renderPosts() {
                 return this.state.list.filter(this.filterAutor).filter(this.filterSerie).filter(this.filterDisciplina).map(lista => {
                         return (
-                                <Jumbotron key={lista.key} className="post d-flex flex-row  p-1">
+                                <Jumbotron key={lista.key} className="post d-flex flex-row m-1 p-1">
                                         <div className="d-none d-sm-block">
                                                 <Autor autor={lista.pautor} disciplinas_autor={lista.pautor_disc} />
                                         </div>
-                                        <div className="pr-3 w-100 d-flex flex-column">
-                                                {console.log(lista.pseries)}
+                                        <div className="pr-3 w-100 d-flex flex-column text-left">
+
                                                 <h5 className="m-0">{lista.ptitulo}</h5>
                                                 <p className="m-0">{lista.pdescricao}</p>
-                                                {this.renderTag(lista.pautor,lista.pdata_ins, lista.pdisciplina,lista.pseries )}
+                                                {this.renderTag(lista.pautor, lista.pdata_ins, lista.pdisciplina, lista.pseries)}
                                                 {/* <Tag autor={lista.pautor}  data={lista.pdata_ins} disciplina={lista.pdisciplina} serie={lista.pseries} /> */}
                                         </div>
                                 </Jumbotron>
                         )
                 })
         }
-   
-        render() {              
+
+        render() {
                 return (<>
+
+                        {this.renderPosts()}
                         
-                        {this.renderPosts()}</>)
+                        </>
+                )
         }
 }
 
